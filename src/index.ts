796a09b2a4a97e80f8a037a953611f098e743960
Alfonso Jose Barroso Barajas <alfonsojbarroso@hotmail.com>
2019-09-13 22:50:12 -0600

    Cambio a puerto 5000

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ class Server {
 
     setParams(): void {
 
-        this.port = process.env.port || 3002;
+        this.port = process.env.port || 5000;
         this.app.set('port', this.port);
         this.app.use(cors());
         this.app.use(express.json());
